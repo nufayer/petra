d79e73390e01ce0fb837d0b0f2e10911196a98e2
nufayer <nufayer6543@gmail.com>
2026-05-18 14:56:45 +0600

    initiated project

---
+++ src/components/Banner.jsx
@@ -0,0 +1,34 @@
+import { Separator } from "@heroui/react";
+
+const Banner = () => {
+  return (
+    <div className="bg-[url('/assets/banner.JPG')] text-white  flex justify-between flex-col items-center  gap-5 h-185 w-full">
+      <div className="p-10 text-center flex justify-center flex-col items-center gap-3.5 flex-1">
+        <h1 className="text-7xl">
+          Discover Your <br /> Next Adventure
+        </h1>
+
+        <p className="text-2xl">
+          Explore breathtaking destinations and create unforgettable memories
+          with our curated travel experiences.
+        </p>
+
+        <div className="flex gap-5">
+          <button className="uppercase bg-cyan-500 px-5 py-3 cursor-pointer">
+            Explore Now
+          </button>
+
+          <button className="uppercase px-5 py-3 bg-white/50 cursor-pointer">
+            View Destination
+          </button>
+        </div>
+      </div>
+
+      <div className=" bg-white/30 flex justify-between gap-5 w-full items-center">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
