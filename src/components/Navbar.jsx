62299e67cb9fbdccb5ea4f6c8833d7a55fc7c2c3
nufayer <nufayer6543@gmail.com>
2026-05-20 21:05:46 +0600

    added login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { authClient } from '@/lib/auth-client';
+import { Avatar, Button } from '@heroui/react';
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+
+    const { 
+        data: session, 
+    } = authClient.useSession() 
+
+    const user = session?.user;
+    console.log({user});
+
     return (
         <nav className='flex justify-between bg-gray-100 p-5'>
             <ul className='flex gap-4 font-bold text-gray-700'>
@@ -15,10 +27,23 @@ const Navbar = () => {
                 <h2 className='font-bold text-4xl text-blue-500'>Petra</h2>
             </div>
 
-            <ul className='flex gap-4 font-bold text-gray-700'>
+            <ul className='flex items-center gap-4 font-bold text-gray-700'>
                 <li><Link href="/profile">Profile</Link></li>
+
+             { user ? <>
+             <li>
+                 <Avatar>
+                        <Avatar.Image alt="John Doe" src={user?.image} />
+                        <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
+                 </Avatar>
+             </li>
+             <li>
+                <Button className="rounded-none bg-red-800" >Logout</Button>
+             </li>
+             </> : <>
                 <li><Link href="/login">Login</Link></li>
                 <li><Link href="/signup">Signup</Link></li>
+             </>}   
             </ul>
         </nav>
     );
