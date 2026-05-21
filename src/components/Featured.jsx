1a7c7ba901404c58fa862ba89fc578e953b3ec8c
nufayer <nufayer6543@gmail.com>
2026-05-21 21:11:22 +0600

    added featured section

---
+++ src/components/Featured.jsx
@@ -0,0 +1,21 @@
+import PetCard from "@/components/PetCard";
+
+const Featured = async () => {
+    const res = await fetch("http://localhost:5000/featured" );
+    const featuredPets = await res.json();
+    console.log({featuredPets})
+    return (
+        <div className="my-20">
+            <div>
+                <h1 className="text-3xl font-bold mb-4 text-blue-500 max-w-7xl mx-auto">Featured Pets</h1>
+            </div>
+
+            <div className="grid grid-cols-4 mt-10 gap-5 max-w-7xl mx-auto">
+                {featuredPets.map((pet) => <PetCard key={pet._id} pet={pet} />)}
+            </div>
+        </div>
+       
+    );
+};
+
+export default Featured;
